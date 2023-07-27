181dbd573ec7c9a6358a9a25c6fd667dbf7a439c
abrihter <bojan.markovic.freelance@gmail.com>
2023-07-27 02:01:01 +0200

    Finish subscriptions

--- beehiiv/subscriptions.py
+++ beehiiv/subscriptions.py
@@ -2,17 +2,17 @@ from beehiiv.endpoint import Endpoint
 
 class Subscriptions(Endpoint):
 
-    def __init__(self, api_key):
+    def __init__(self, api_key, publicationId):
         super().__init__(api_key)
+        self.endpoint = "/publications/{}/subscriptions"
+        self.publicationId = publicationId
 
-    def create(self, publicationId, email, reactivate_existing=None,
-               send_welcome_email=None, utm_source=None, utm_medium=None,
-               utm_campaign=None, referring_site=None, referral_code=None,
-               custom_fields=[]):
+    def create(self, email, reactivate_existing=None, send_welcome_email=None,
+               utm_source=None, utm_medium=None, utm_campaign=None,
+               referring_site=None, referral_code=None, custom_fields=[]):
         '''create'''
         return self._make_call(
-            "/publications/{}/subscriptions",
-            [publicationId],
+            [self.publicationId],
             data={
                 "email": email,
                 "reactivate_existing": reactivate_existing,
@@ -27,9 +27,35 @@ class Subscriptions(Endpoint):
             method="POST",
         ).json()
 
-    def index(self, publicationId):
+    def index(self):
         '''index'''
         return self._make_call(
-            "/publications/{}/subscriptions",
-            [publicationId],
+            [self.publicationId],
+        ).json()
+
+    def show(self, subscriptionId):
+        '''show'''
+        return self._make_call(
+            endpoint=self.endpoint + "/{}"
+            [self.publicationId, subscriptionId],
+        ).json()
+
+    def update(self, subscriptionId, unsubscribe=None, custom_fields=[]):
+        '''update'''
+        return self._make_call(
+            endpoint=self.endpoint + "/{}"
+            [self.publicationId, subscriptionId],
+            data={
+                "unsubscribe": unsubscribe,
+                "custom_fields": custom_fields,
+            },
+            method="PUT",
+        ).json()
+
+    def delete(self, subscriptionId):
+        '''delete'''
+        return self._make_call(
+            endpoint=self.endpoint + "/{}"
+            [self.publicationId, subscriptionId],
+            method="DELETE",
         ).json()
